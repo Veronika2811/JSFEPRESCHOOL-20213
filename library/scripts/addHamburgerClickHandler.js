d4ef1a94f4e1fa529737ddd8ccf61a78d28e748b
Veranika Smiayun <smeyun@list.ru>
2023-08-01 18:38:26 +0300

    feat: add burger menu, implemented adaptive

---
+++ library/scripts/addHamburgerClickHandler.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger');
+const headerNavigation = document.querySelector('.header__navigation');
+const maskContent = document.querySelector('.mask-content');
+const body = document.querySelector('body');
+
+const toggleMenu = () => {
+  if (window.innerWidth > 1180) return;
+
+  hamburger.classList.toggle('open');
+  headerNavigation.classList.toggle('open');
+  maskContent.classList.toggle('open');
+  body.classList.toggle('open');
+};
+
+const addHamburgerClickHandler = () => {
+  hamburger.addEventListener('click', toggleMenu);
+  maskContent.addEventListener('click', toggleMenu);
+
+  headerNavigation.addEventListener('click', (e) => {
+    if (e.target.classList.contains('navigation__link')) toggleMenu();
+  });
+};
+
+export default addHamburgerClickHandler;
