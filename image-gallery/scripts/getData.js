606744492a769b8b9e30547a65d246739da89716
Veranika Smiayun <smeyun@list.ru>
2023-09-28 22:06:12 +0300

    feat: add warning about limiting requests

--- image-gallery/scripts/getData.js
+++ image-gallery/scripts/getData.js
@@ -5,6 +5,13 @@ let totalPages;
 const getData = async (query = 'pink', page) => {
   const url = `https://api.unsplash.com/search/photos?&client_id=${ACCESS_KEY}&query=${query}&page=${page}&per_page=30&orientation=landscape`;
   const res = await fetch(url);
+
+  if (res.status === 403) {
+    return alert(
+      'Демо доступ разрешает 50 запросов в час. Попробуйте проверить работу через час.'
+    );
+  }
+
   const data = await res.json();
   totalPages = data.total_pages;
   return data;
